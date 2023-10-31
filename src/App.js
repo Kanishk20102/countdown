97ea922476deae6a548e7008784aa7a14ef72c23
Kanishk Singh <kanishk@basicx.in>
2023-10-31 16:42:28 +0530

    [Added] pause timer when user types in the input field

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     function handleInput(e) {
         setInputValue(e.target.value)
         setTime(e.target.value)
+        setIsPaused(true)
     }
 	function pauseTimer() {
 		setIsPaused(true);
